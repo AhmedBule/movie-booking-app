76b81929d477b5490c2e0a8593d89fb8a18d20f1
Ahmed Bule <ahmedbule01@gmail.com>
2021-03-12 19:46:39 +0300

    added the documents for all the classes and id

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container')// It will just selector one element. 
+const seats = document.querySelectorAll('.row .seat:not(.occupied)') // Query selectorAll grabs and puts them in a node list.
+const count = document.getElementById('count') // We use the getElementByID because count is id in the span. 
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = movieSelect.value;
+
+
+
